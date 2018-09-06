bacc6e8f1be3b8e890382d1eb9808b0bf16fadf3
unknown <catsheue@gmail.com>
2018-09-06 18:28:43 +0800

    touch a little :lipstick:

--- src/Contact.js
+++ src/Contact.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
+import LunaCircle from "./img/luna.png";
+
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <h2>Contact</h2>
-                <p>Luna Goodnight</p>
+            <div className="react__contact">
+                <h2 className="react__contactit">Contact</h2>
+                <img src={LunaCircle} alt=""/>
+                <p className="react__contactName">Luna Goodnight</p>
+                <p>Hire Me Please :</p>
                 <p>catsheue@gmail.com</p>
             </div>
         //    ↓不加分號沒關係吧???
-        );
+        )
     }
 }
 export default Contact;
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import Emoji from "./Emoji";
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h2>HELLO</h2>
-                <p>Luna is cute😄</p>
+                <p>Luna is cute <Emoji symbol="😄"/></p>
             </div>
         )
     }
