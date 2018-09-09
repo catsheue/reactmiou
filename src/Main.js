ab502047f685119e28b9ef7e5ace6b00d4b16ede
unknown <catsheue@gmail.com>
2018-09-09 18:51:59 +0800

    add Taiwan news :sparkles:

--- src/Main.js
+++ src/Main.js
@@ -1,47 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Route, NavLink, HashRouter
 } from "react-router-dom";
 import Home from "./Home";
 import Stuff from "./Stuff";
-import Todo from "./Todo";
+//import Todo from "./Todo";
+
 import Contact from "./Contact";
 import Emoji from "./Emoji";
 
+import AnimateHeight from 'react-animate-height';
 
-class Toggle extends React.Component {
+
+
+import RenderJSON from "./stuff/renderjson/Renderjson";
+import TaiwanNews from "./stuff/taiwannews/TaiwanNews";
+
+
+
+class Main extends Component {
+
     constructor(props) {
         super(props);
-        this.state = {isToggleOn: true};
-
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            height: 0,
+        };
     }
 
-    handleClick() {
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }));
-    }
+    toggle = () => {
+        const {height} = this.state;
+
+        this.setState({
+            height: height === 0 ? 'auto' : 0,
+        });
+    };
 
     render() {
-        return (
-            <button onClick={this.handleClick}>
-                {this.state.isToggleOn ? 'ON' : 'OFF'}
-            </button>
-        );
-    }
-}
-
-class Main extends Component{
-    render(){
+        const {height} = this.state;
         return (
             <HashRouter>
                 <div className="react">
                     <h1 className="react__title">
                         <Emoji symbol="🐑"/>
                         <Emoji symbol="🎃"/>
-                         React Rocks
+                        React Rocks
                         <Emoji symbol="😺"/>
                         <Emoji symbol="🐑"/>
                     </h1>
@@ -51,21 +53,31 @@ class Main extends Component{
                                 <NavLink className="react__anchor" to="/">Home</NavLink>
                             </li>
                             <li>
-                                <NavLink className="react__anchor" to="/stuff">Stuff</NavLink>
-                                <ul className="react__submenu">
-                                    <li>
-                                        <NavLink className="react__subanchor" to="/todo">Todo App</NavLink>
-                                    </li>
-                                </ul>
+                                <button className="react__anchor" onClick={this.toggle}>
+                                    Skills
+                                </button>
+                                <AnimateHeight
+                                    duration={500}
+                                    height={height} // see props documentation bellow
+                                >
+
+                                    <ul className="react__submenu">
+                                        <li>
+                                            <NavLink className="react__subanchor" to="/renderjson">Render JSON</NavLink>
+                                            <NavLink className="react__subanchor" to="/taiwannews">Taiwan News AJAX</NavLink>
+                                        </li>
+                                    </ul>
+                                </AnimateHeight>
                             </li>
                             <li>
                                 <NavLink className="react__anchor" to="/contact">Contact</NavLink>
                             </li>
                         </ul>
                         <div className="react__content">
-                            <Route exact path="/" component={Home} />
+                            <Route exact path="/" component={Home}/>
                             <Route path="/stuff" component={Stuff}/>
-                            <Route path="/todo" component={Todo}/>
+                            <Route path="/renderjson" component={RenderJSON}/>
+                            <Route path="/taiwannews" component={TaiwanNews}/>
                             <Route path="/contact" component={Contact}/>
                         </div>
                     </div>
---
+++ src/stuff/taiwannews/TaiwanNews.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./../../css/stuff/newsapi.scss";
+
+class Attribution extends React.Component {
+    render() {
+        return (
+            <div>
+                <small><em>Powered by NewsApi.org</em></small>
+            </div>
+        )
+    }
+}
+
+class News extends React.Component {
+    render() {
+        return <div>
+            <ul className="list-unstyled">
+                {this.props.news.map((newsItem) =>
+                    <li className="news-item">
+                        <a target="_blank" href={newsItem.url}>
+                            <div className="cf">
+                                <div className="newsapi__pic">
+                                    <div className="newsapi__thumbnail"><img alt="news" src={newsItem.urlToImage}/></div>
+                                </div>
+                                <div className="newsapi__con">
+                                    <h4>{newsItem.title}</h4>
+                                    {newsItem.description}</div>
+                            </div>
+                        </a>
+                    </li>)}
+            </ul>
+            <Attribution></Attribution>
+        </div>;
+    }
+}
+
+class NewsContainer extends React.Component {
+    url = 'https://newsapi.org/v2/top-headlines?country=tw&apiKey=d0333c98698e4f6b818186a29bf10625';
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: []
+        }
+    }
+
+    componentDidMount() {
+        var self = this;
+        //console.log('this.url=', this.url);
+        fetch(this.url)
+            .then(function (response) {
+                return response.json()
+            }).then(function (json) {
+            //console.log('parsed json', json)
+            if (json.status === 'ok') {
+                self.setState({
+                    news: json.articles
+                });
+            } else {
+                alert(json.status);
+            }
+        }).catch(function (ex) {
+            console.log('parsing failed', ex)
+        })
+    }
+
+    render() {
+        return <div>
+
+            <News news={this.state.news}/>
+        </div>;
+    }
+}
+
+class TaiwanNews extends React.Component {
+    render() {
+        return <div className="newsapi">
+            <h1>Taiwan Headline News</h1>
+            <NewsContainer/>
+        </div>;
+    }
+}
+
+
+export default TaiwanNews;
